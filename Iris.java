9360b4b262e201222db22ad94a14d6c1a3594d5d
shawmori <shaw.david.mori@gmail.com>
2017-03-24 13:05:14 +1300

    Add files via upload

--- Iris.java
+++ Iris.java
@@ -1,10 +1,10 @@
 
 public class Iris {
-	
+
 	private double sepalL, sepalW, petalL, petalW;
 	private String label;
 	private String classification;
-	
+
 	public Iris(double sepalL, double sepalW, double petalL, double petalW, String label){
 		this.sepalL = sepalL;
 		this.sepalW = sepalW;
@@ -12,45 +12,56 @@ public class Iris {
 		this.petalW = petalW;
 		this.label = label;
 	}
-	
-	public double getEuclideanDistance(Iris i){
+
+	public double getEuclideanDistance(Iris i, double[] ranges){
 		double w1 = sepalL;				//
 		double x1 = sepalW;				//
 		double y1 = petalL;				//
 		double z1 = petalW;				//	Simple variable names for easier readability
-		
+
 		double w2 = i.getSepalL();		//
 		double x2 = i.getSepalW();		//
 		double y2 = i.getPetalL();		//
 		double z2 = i.getPetalW();		//
-		
+
 		double w3 = Math.pow(w1 - w2, 2);
 		double x3 = Math.pow(x1 - x2, 2);
 		double y3 = Math.pow(y1 - y2, 2);
 		double z3 = Math.pow(z1 - z2, 2);
 		
-		double eucDist = Math.sqrt(w3 + x3 + y3 + z3);
-		
+		double r0 = Math.pow(ranges[0], 2);
+		double r1 = Math.pow(ranges[1], 2);
+		double r2 = Math.pow(ranges[2], 2);
+		double r3 = Math.pow(ranges[3], 2);
+
+		double eucDist = Math.sqrt((w3/r0) + (x3/r1) + (y3/r2) + (z3/r3));
+
 		return eucDist;
 	}
-	
+
+	public boolean correct(){
+		if(label.equals(classification))
+			return true;
+		return false;
+	}
+
 	public void setClassification(String s){
 		classification = s;
 	}
-	
-	private double getPetalW() {
+
+	public double getPetalW() {
 		return petalW;
 	}
 
-	private double getPetalL() {
+	public double getPetalL() {
 		return petalL;
 	}
 
-	private double getSepalW() {
+	public double getSepalW() {
 		return sepalW;
 	}
 
-	private double getSepalL() {
+	public double getSepalL() {
 		return sepalL;
 	}
 
@@ -59,7 +70,7 @@ public class Iris {
 	}
 
 	public void printResults() {
-		
+
 		System.out.println("Label: " + label);
 		System.out.println("Classification: " + classification);
 		System.out.println("");
--- Main.java
+++ Main.java
@@ -2,44 +2,82 @@ import java.io.File;
 import java.io.IOException;
 import java.util.*;
 
+import javax.swing.plaf.synth.SynthSpinnerUI;
+
 public class Main {
-	
+
 	private ArrayList<Iris> trainingData = new ArrayList<Iris>();
 	private ArrayList<Iris> testData = new ArrayList<Iris>();
-	
+
 	int k;
-	
-	
+
+
 	public Main(String trainingData, String testData, int k){
 		readData(trainingData, 0);
 		readData(testData, 1);
 		this.k = k;
-		
+
 		classify();
 	}
-	
+
 	public void classify(){
 		double[] distances;
-	
+		int[] indices;
+
 		for(Iris i : testData){
 			distances = calculateDistances(i);
 			if(k == 1){
-			int index = getNearestNeighbour(distances);
-			setClassification(i, index);
+				int index = getNearestNeighbour(distances);
+				setClassification(i, index);
 			}
-			else
-				getNearestNeighbours(distances);
+			else{
+				indices = getNearestNeighbours(distances);
+				String classification = findMajority(indices);
+				i.setClassification(classification);
+			}
 		}
-		
+
 		for(Iris i : testData){
 			i.printResults();
 		}
-		
+
+		double correct = 0;
+		for(Iris i : testData){
+			if(i.correct())
+				correct++;
+		}
+
+		double percentage = correct / testData.size() * 100;
+		System.out.println(percentage + "% correct.");
+
 	}
-	
+
+	private String findMajority(int[] indices) {
+		HashMap<String, Integer> labels = new HashMap<String, Integer>();
+
+		for(int i : indices){
+			String label = trainingData.get(i).getLabel();
+			if(!labels.containsKey(label)){
+				labels.put(label, 1);
+			}else{
+				labels.put(label, labels.get(label) + 1);
+			}
+		}
+		int max = 0;
+		String classification = null;
+
+		for(String s : labels.keySet()){
+			if(labels.get(s) > max){
+				classification = s;
+				max = labels.get(s);
+			}
+		}
+		return classification;
+	}
+
 	private void setClassification(Iris i, int index) {
 		String classification = trainingData.get(index).getLabel();
-		i.setClassification(classification);		
+		i.setClassification(classification);
 	}
 
 	public int getNearestNeighbour(double[] distances){
@@ -53,11 +91,18 @@ public class Main {
 		}
 		return index;
 	}
-	
-	public void getNearestNeighbours(double[] distances){
-		
+
+	public int[] getNearestNeighbours(double[] distances){
+		int[] indices = new int[k];
+
+		for(int i = 0; i < k; i++){
+			indices[i] = getNearestNeighbour(distances);
+			distances[indices[i]] = Double.MAX_VALUE;
+		}
+
+		return indices;
 	}
-	
+
 	/**
 	 * Calculates the Euclidean distance between the Iris parameter test and every training Iris in the training data.
 	 * @param test
@@ -66,14 +111,51 @@ public class Main {
 	public double[] calculateDistances(Iris test){
 		double[] distances = new double[trainingData.size()];
 		for(int i = 0; i < distances.length; i++){
-				distances[i] = test.getEuclideanDistance(trainingData.get(i));
+				distances[i] = test.getEuclideanDistance(trainingData.get(i), getRanges());
 			}
 		return distances;
 	}
-	
+
+	public double[] getRanges() {
+		double min = Double.MIN_VALUE;
+		double max = Double.MAX_VALUE;
+
+		double[] highRanges = new double[]{min, min, min, min};
+		double[] lowRanges = new double[]{max, max, max, max};
+
+		for(Iris i : trainingData){
+
+			double[] theseFeatures = new double[]{i.getSepalL(), i.getSepalW(), i.getPetalL(), i.getPetalW()};
+
+			if(theseFeatures[0] < lowRanges[0])
+				lowRanges[0] = theseFeatures[0];
+			else if(theseFeatures[0] > highRanges[0])
+				highRanges[0] = theseFeatures[0];
+
+			if(theseFeatures[1] < lowRanges[1])
+				lowRanges[1] = theseFeatures[1];
+			else if(theseFeatures[1] > highRanges[1])
+				highRanges[1] = theseFeatures[1];
+
+			if(theseFeatures[2] < lowRanges[2])
+				lowRanges[2] = theseFeatures[2];
+			else if(theseFeatures[2] > highRanges[2])
+				highRanges[2] = theseFeatures[2];
+
+			if(theseFeatures[3] < lowRanges[3])
+				lowRanges[3] = theseFeatures[3];
+			else if(theseFeatures[3] > highRanges[3])
+				highRanges[3] = theseFeatures[3];
+		}
+
+		double[] finalRanges = new double[]{highRanges[0] - lowRanges[0], highRanges[1] - lowRanges[1], highRanges[2] - lowRanges[2], highRanges[3] - lowRanges[3]};
+		return finalRanges;
+
+	}
+
 	/**
 	 * Reads data from a file and creates the training and test collections of irises.
-	 * 
+	 *
 	 * @param filename
 	 * @param i (0 for training set, 1 for test set)
 	 */
@@ -86,9 +168,9 @@ public class Main {
 				double petalL = sc.nextDouble();
 				double petalW = sc.nextDouble();
 				String label = sc.next();
-				
+
 				if(i == 0)
-					trainingData.add(new Iris(sepalL, sepalW, petalL, petalW, label));	
+					trainingData.add(new Iris(sepalL, sepalW, petalL, petalW, label));
 				else if(i == 1)
 					testData.add(new Iris(sepalL, sepalW, petalL, petalW, label));
 			}
@@ -97,18 +179,31 @@ public class Main {
 			System.out.println(e.getMessage());
 		}
 	}
-	
-	
-	
-	
-	
-	
+
+
+
+
+
+
 	public static void main(String[] args){
 		try {
-			int k = Integer.parseInt(args[2]);	//Parse string k-value to an integer
+			Scanner sc = new Scanner(System.in);
+			System.out.print("Choose a value for k: ");
+			int k;
+			if(sc.hasNextInt()){
+				k = sc.nextInt();
+			}else{
+				sc.close();
+				throw new IllegalArgumentException();
+			}
+			if((k % 2) == 0){
+				sc.close();
+				throw new IllegalArgumentException();
+			}
 			new Main(args[0], args[1], k);
-         } catch (NumberFormatException e) {
-            System.err.println("Failed trying to parse a non-numeric argument, " + args[2]);
-         }		
+			sc.close();
+         } catch (IllegalArgumentException e){
+        	 System.err.println("Value for k must be an odd number");
+         }
 	}
 }
